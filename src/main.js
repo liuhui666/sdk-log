65247e0b4c1f27f5811937f298c15b351d95f05f
liuhui16 <liuhui16@100tal.com>
2019-12-04 14:43:58 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
-import App from './App.vue';
+import ElementUI from 'element-ui';
+import Home from './views/Home.vue';
 import router from './router';
 import store from './store';
+import 'element-ui/lib/theme-chalk/index.css';// 引用ui样式
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: h => h(Home),
 }).$mount('#app');
